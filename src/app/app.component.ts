17285631a88bc7b04409ffb0572828f1054b1430
Hector Isaac Roman Vazquez <72269944+RomanDrum821@users.noreply.github.com>
2023-12-01 19:24:46 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { VincularComponent } from './vincular/vincular.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, VincularComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'CBDF_HIRV_Ejercicio11';
